f3f15a98676896461f5659ae31564836db734802
Azulox <dylou49@gmail.com>
2023-03-23 22:43:09 +0100

    plein de trucs

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<head>
+    <head>
+        <title>Je sais pas encore</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="icon" href="img/Brooklyn_Nine-Nine_Logo.png">
+        
+    </head>
+</head>
+
+<html> 
+<body>
+
+<!--<div>
+  <p class="nom">brooklyn nine nine lol</p>
+  <button onclick="generation()">Générer</button>
+</div>
+
+<script>
+function generation() 
+        { 
+            $x = rand(0, 4);
+            switch($x) {
+            case 0:
+                echo 'Saison 1 - Episode 01';
+                break;
+            case 1:
+                echo 'Saison 1 - Episode 02';
+                break;
+            case 2:
+                echo 'Saison 1 - Episode 03';
+                break;
+            case 3:
+                echo 'Saison 1 - Episode 04';
+                break;
+            case 4:
+                echo 'Saison 1 - Episode 05';
+                break;
+            default:
+                echo 'bug pas ça marche pas lol';
+            }
+        }
+</script>
+    -->
+
+    <form>
+<input type="button" value="Démarrer la machine">
+</form>
+<p>test </p>
+<script>
+var btn = document.querySelector('input');
+var txt = document.querySelector('p');
+
+
+function updateBtn() {
+            switch() {
+            case 0:
+                echo 'Saison 1 - Episode 01';
+                break;
+            case 1:
+                echo 'Saison 1 - Episode 02';
+                break;
+            case 2:
+                echo 'Saison 1 - Episode 03';
+                break;
+            case 3:
+                echo 'Saison 1 - Episode 04';
+                break;
+            case 4:
+                echo 'Saison 1 - Episode 05';
+                break;
+            default:
+                echo 'bug pas ça marche pas lol';
+            }
+}
+</script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,8 @@
+function script()= Math.floor(Math.random()*5)
+            switch(aleatoire){
+            case 0:document.write("phrase1" );break;
+            case 1:document.write("phrase2" );break;
+            case 2:document.write("phrase3" );break;
+            case 3:document.write("phrase4" );break;
+            case 4:document.write("phrase5" );break;
+            }
