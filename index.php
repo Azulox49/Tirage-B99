91c2bf8ae8b619ffa8e88d0432f5abb6137ad0c6
Azulox <dylou49@gmail.com>
2023-03-24 12:28:46 +0100

    tout

--- index.php
+++ index.php
@@ -12,6 +12,45 @@
 <html> 
 <body>
 
+<div class="tous">
+
+
+<button class="bouton" onclick="style.display = 'none'">
+AFFICHER
+</button>
+
+
+<strong>
+<div class="php">
+<?php 
+    $x = rand(0, 4);
+            switch($x) {
+            case 0:
+                echo 'Saison 1 - Episode 01';
+                break;
+            case 1:
+                echo 'Saison 1 - Episode 02';
+                break;
+            case 2:
+                echo 'Saison 1 - Episode 03';
+                break;
+            case 3:
+                echo 'Saison 1 - Episode 04';
+                break;
+            case 4:
+                echo 'Saison 1 - Episode 05';
+                break;
+            default:
+                echo 'bug pas ça marche pas lol';
+            }
+    
+?>
+</div>
+        </div>
+        </strong>
+
+
+
 <!--<div>
   <p class="nom">brooklyn nine nine lol</p>
   <button onclick="generation()">Générer</button>
@@ -42,18 +81,23 @@ function generation()
             }
         }
 </script>
-    -->
+    
 
-    <form>
+<form>
 <input type="button" value="Démarrer la machine">
 </form>
+
+
 <p>test </p>
-<script>
-var btn = document.querySelector('input');
-var txt = document.querySelector('p');
 
 
-function updateBtn() {
+
+    <script>
+        var btn = document.querySelector('input');
+        var txt = document.querySelector('p');
+
+
+        function updateBtn() {
             switch() {
             case 0:
                 echo 'Saison 1 - Episode 01';
@@ -73,7 +117,10 @@ function updateBtn() {
             default:
                 echo 'bug pas ça marche pas lol';
             }
-}
-</script>
+        }
+    </script>
+
+  
+
 </body>
 </html>
