c46e5daa647e3c7123068b2673e3d688e781e89f
Azulox <dylou49@gmail.com>
2023-03-26 21:10:02 +0200

    Fini

--- index.php
+++ index.php
@@ -14,24 +14,19 @@
 
 <img src="img/logo2.png" class="logo2">
 
-<strong>
-<div class="wrap">
-<strong>
 
-    <!--
-    <button class="favorite styled" onclick="style.display = 'none'">
-        <strong>AFFICHER</strong>
-    </button>-->
-    
+<div class="wrap">
+
     <div class="button">
         <button class="button2" onclick="style.display = 'none'">
             <strong>AFFICHER</strong>
         </button>
     </div>
 
+<strong>
     <div class="php">
         <?php 
-            $x = rand(0, 4);
+            $x = rand(0, 152);
                 switch($x) {
                 case 0:
                     echo 'Saison 1 - Episode 01';
@@ -48,89 +43,460 @@
                 case 4:
                     echo 'Saison 1 - Episode 05';
                     break;
+                case 5:
+                    echo 'Saison 1 - Episode 06';
+                    break;
+                case 6:
+                    echo 'Saison 1 - Episode 07';
+                    break;
+                case 7:
+                    echo 'Saison 1 - Episode 08';
+                    break;
+                case 8:
+                    echo 'Saison 1 - Episode 09';
+                    break;
+                case 9:
+                    echo 'Saison 1 - Episode 10';
+                    break;
+                case 10:
+                    echo 'Saison 1 - Episode 11';
+                    break;
+                case 11:
+                    echo 'Saison 1 - Episode 12';
+                    break;
+                case 12:
+                    echo 'Saison 1 - Episode 13';
+                    break;
+                case 13:
+                    echo 'Saison 1 - Episode 14';
+                    break;
+                case 14:
+                    echo 'Saison 1 - Episode 15';
+                    break;
+                case 15:
+                    echo 'Saison 1 - Episode 16';
+                    break;
+                case 16:
+                    echo 'Saison 1 - Episode 17';
+                    break;
+                case 17:
+                    echo 'Saison 1 - Episode 18';
+                    break;
+                case 18:
+                    echo 'Saison 1 - Episode 19';
+                    break;
+                case 19:
+                    echo 'Saison 1 - Episode 20';
+                    break;
+                case 20:
+                    echo 'Saison 1 - Episode 21';
+                    break;
+                case 21:
+                    echo 'Saison 1 - Episode 22';
+                    break;
+                case 22:
+                    echo 'Saison 2 - Episode 01';
+                    break;
+                case 23:
+                    echo 'Saison 2 - Episode 02';
+                    break;
+                case 24:
+                    echo 'Saison 2 - Episode 03';
+                    break;
+                case 25:
+                    echo 'Saison 2 - Episode 04';
+                    break;
+                case 26:
+                    echo 'Saison 2 - Episode 05';
+                    break;
+                case 27:
+                    echo 'Saison 2 - Episode 06';
+                    break;
+                case 28:
+                    echo 'Saison 2 - Episode 07';
+                    break;
+                case 29:
+                    echo 'Saison 2 - Episode 08';
+                    break;
+                case 30:
+                    echo 'Saison 2 - Episode 09';
+                    break;
+                case 31:
+                    echo 'Saison 2 - Episode 10';
+                    break;
+                case 32:
+                    echo 'Saison 2 - Episode 11';
+                    break;
+                case 33:
+                    echo 'Saison 2 - Episode 12';
+                    break;
+                case 34:
+                    echo 'Saison 2 - Episode 13';
+                    break;
+                case 35:
+                    echo 'Saison 2 - Episode 14';
+                    break;
+                case 36:
+                    echo 'Saison 2 - Episode 15';
+                    break;
+                case 37:
+                    echo 'Saison 2 - Episode 16';
+                    break;
+                case 38:
+                    echo 'Saison 2 - Episode 17';
+                    break;
+                case 39:
+                    echo 'Saison 2 - Episode 18';
+                    break;
+                case 40:
+                    echo 'Saison 2 - Episode 19';
+                    break;
+                case 41:
+                    echo 'Saison 2 - Episode 20';
+                    break;
+                case 42:
+                    echo 'Saison 2 - Episode 21';
+                    break;
+                case 43:
+                    echo 'Saison 2 - Episode 22';
+                    break;
+                case 44:
+                    echo 'Saison 2 - Episode 23';
+                    break;
+                case 45:
+                    echo 'Saison 3 - Episode 01';
+                    break;
+                case 46:
+                    echo 'Saison 3 - Episode 02';
+                    break;
+                case 47:
+                    echo 'Saison 3 - Episode 03';
+                    break;
+                case 48:
+                    echo 'Saison 3 - Episode 04';
+                    break;
+                case 49:
+                    echo 'Saison 3 - Episode 05';
+                    break;
+                case 50:
+                    echo 'Saison 3 - Episode 06';
+                    break;
+                case 51:
+                    echo 'Saison 3 - Episode 07';
+                    break;
+                case 52:
+                    echo 'Saison 3 - Episode 08';
+                    break;
+                case 53:
+                    echo 'Saison 3 - Episode 09';
+                    break;
+                case 54:
+                    echo 'Saison 3 - Episode 10';
+                    break;
+                case 55:
+                    echo 'Saison 3 - Episode 11';
+                    break;
+                case 56:
+                    echo 'Saison 3 - Episode 12';
+                    break;
+                case 57:
+                    echo 'Saison 3 - Episode 13';
+                    break;
+                case 58:
+                    echo 'Saison 3 - Episode 14';
+                    break;
+                case 59:
+                    echo 'Saison 3 - Episode 15';
+                    break;
+                case 60:
+                    echo 'Saison 3 - Episode 16';
+                    break;
+                case 61:
+                    echo 'Saison 3 - Episode 17';
+                    break;
+                case 62:
+                    echo 'Saison 3 - Episode 18';
+                    break;
+                case 63:
+                    echo 'Saison 3 - Episode 19';
+                    break;
+                case 64:
+                    echo 'Saison 3 - Episode 20';
+                    break;
+                case 65:
+                    echo 'Saison 3 - Episode 21';
+                    break;
+                case 66:
+                    echo 'Saison 3 - Episode 22';
+                    break;
+                case 67:
+                    echo 'Saison 3 - Episode 23';
+                    break;
+                case 68:
+                    echo 'Saison 4 - Episode 01';
+                    break;
+                case 69:
+                    echo 'Saison 4 - Episode 02';
+                    break;
+                case 70:
+                    echo 'Saison 4 - Episode 03';
+                    break;
+                case 71:
+                    echo 'Saison 4 - Episode 04';
+                    break;
+                case 72:
+                    echo 'Saison 4 - Episode 05';
+                    break;
+                case 73:
+                    echo 'Saison 4 - Episode 06';
+                    break;
+                case 74:
+                    echo 'Saison 4 - Episode 07';
+                    break;
+                case 75:
+                    echo 'Saison 4 - Episode 08';
+                    break;
+                case 76:
+                    echo 'Saison 4 - Episode 09';
+                    break;
+                case 77:
+                    echo 'Saison 4 - Episode 10';
+                    break;
+                case 78:
+                    echo 'Saison 4 - Episode 11';
+                    break;
+                case 79:
+                    echo 'Saison 4 - Episode 12';
+                    break;
+                case 80:
+                    echo 'Saison 4 - Episode 13';
+                    break;
+                case 81:
+                    echo 'Saison 4 - Episode 14';
+                    break;
+                case 82:
+                    echo 'Saison 4 - Episode 15';
+                    break;
+                case 83:
+                    echo 'Saison 4 - Episode 16';
+                    break;
+                case 84:
+                    echo 'Saison 4 - Episode 17';
+                    break;
+                case 85:
+                    echo 'Saison 4 - Episode 18';
+                    break;
+                case 86:
+                    echo 'Saison 4 - Episode 19';
+                    break;
+                case 87:
+                    echo 'Saison 4 - Episode 20';
+                    break;
+                case 88:
+                    echo 'Saison 4 - Episode 21';
+                    break;
+                case 89:
+                    echo 'Saison 4 - Episode 22';
+                    break;
+                case 90:
+                    echo 'Saison 5 - Episode 01';
+                    break;
+                case 91:
+                    echo 'Saison 5 - Episode 02';
+                    break;
+                case 92:
+                    echo 'Saison 5 - Episode 03';
+                    break;
+                case 93:
+                    echo 'Saison 5 - Episode 04';
+                    break;
+                case 94:
+                    echo 'Saison 5 - Episode 05';
+                    break;
+                case 95:
+                    echo 'Saison 5 - Episode 06';
+                    break;
+                case 96:
+                    echo 'Saison 5 - Episode 07';
+                    break;
+                case 97:
+                    echo 'Saison 5 - Episode 08';
+                    break;
+                case 98:
+                    echo 'Saison 5 - Episode 09';
+                    break;
+                case 99:
+                    echo 'Saison 5 - Episode 10';
+                    break;
+                case 100:
+                    echo 'Saison 5 - Episode 11';
+                    break;
+                case 101:
+                    echo 'Saison 5 - Episode 12';
+                    break;
+                case 102:
+                    echo 'Saison 5 - Episode 13';
+                    break;
+                case 103:
+                    echo 'Saison 5 - Episode 14';
+                    break;
+                case 104:
+                    echo 'Saison 5 - Episode 15';
+                    break;
+                case 105:
+                    echo 'Saison 5 - Episode 16';
+                    break;
+                case 106:
+                    echo 'Saison 5 - Episode 17';
+                    break;
+                case 107:
+                    echo 'Saison 5 - Episode 18';
+                    break;
+                case 108:
+                    echo 'Saison 5 - Episode 19';
+                    break;
+                case 109:
+                    echo 'Saison 5 - Episode 20';
+                    break;
+                case 110:
+                    echo 'Saison 5 - Episode 21';
+                    break;
+                case 111:
+                    echo 'Saison 5 - Episode 22';
+                    break;
+                case 112:
+                    echo 'Saison 6 - Episode 01';
+                    break;
+                case 113:
+                    echo 'Saison 6 - Episode 02';
+                    break;
+                case 114:
+                    echo 'Saison 6 - Episode 03';
+                    break;
+                case 115:
+                    echo 'Saison 6 - Episode 04';
+                    break;
+                case 116:
+                    echo 'Saison 6 - Episode 05';
+                    break;
+                case 117:
+                    echo 'Saison 6 - Episode 06';
+                    break;
+                case 118:
+                    echo 'Saison 6 - Episode 07';
+                    break;
+                case 119:
+                    echo 'Saison 6 - Episode 08';
+                    break;
+                case 120:
+                    echo 'Saison 6 - Episode 09';
+                    break;
+                case 121:
+                    echo 'Saison 6 - Episode 10';
+                    break;
+                case 122:
+                    echo 'Saison 6 - Episode 11';
+                    break;
+                case 123:
+                    echo 'Saison 6 - Episode 12';
+                    break;
+                case 124:
+                    echo 'Saison 6 - Episode 13';
+                    break;
+                case 125:
+                    echo 'Saison 6 - Episode 14';
+                    break;
+                case 126:
+                    echo 'Saison 6 - Episode 15';
+                    break;
+                case 127:
+                    echo 'Saison 6 - Episode 16';
+                    break;
+                case 128:
+                    echo 'Saison 6 - Episode 17';
+                    break;
+                case 129:
+                    echo 'Saison 6 - Episode 18';
+                    break;
+                case 130:
+                    echo 'Saison 7 - Episode 01';
+                    break;
+                case 131:
+                    echo 'Saison 7 - Episode 02';
+                    break;
+                case 132:
+                    echo 'Saison 7 - Episode 03';
+                    break;
+                case 133:
+                    echo 'Saison 7 - Episode 04';
+                    break;
+                case 134:
+                    echo 'Saison 7 - Episode 05';
+                    break;
+                case 135:
+                    echo 'Saison 7 - Episode 06';
+                    break;
+                case 136:
+                    echo 'Saison 7 - Episode 07';
+                    break;
+                case 137:
+                    echo 'Saison 7 - Episode 08';
+                    break;
+                case 138:
+                    echo 'Saison 7 - Episode 09';
+                    break;
+                case 139:
+                    echo 'Saison 7 - Episode 10';
+                    break;
+                case 140:
+                    echo 'Saison 7 - Episode 11';
+                    break;
+                case 141:
+                    echo 'Saison 7 - Episode 12';
+                    break;
+                case 142:
+                    echo 'Saison 7 - Episode 13';
+                    break;
+                case 143:
+                    echo 'Saison 8 - Episode 01';
+                    break;
+                case 144:
+                    echo 'Saison 8 - Episode 02';
+                    break;
+                case 145:
+                    echo 'Saison 8 - Episode 03';
+                    break;
+                case 146:
+                    echo 'Saison 8 - Episode 04';
+                    break;
+                case 147:
+                    echo 'Saison 8 - Episode 05';
+                    break;
+                case 148:
+                    echo 'Saison 8 - Episode 06';
+                    break;
+                case 149:
+                    echo 'Saison 8 - Episode 07';
+                    break;
+                case 150:
+                    echo 'Saison 8 - Episode 08';
+                    break;
+                case 151:
+                    echo 'Saison 8 - Episode 09';
+                    break;
+                case 152:
+                    echo 'Saison 8 - Episode 10';
+                    break;
                 default:
                     echo 'bug pas ça marche pas lol';
             }
         ?>
     </div>
-
 </strong>
+
 </div>     
 
 
 
 </body>
 </html>
-
-
-<!--<div>
-  <p class="nom">brooklyn nine nine lol</p>
-  <button onclick="generation()">Générer</button>
-</div>
-
-<script>
-function generation() 
-        { 
-            $x = rand(0, 4);
-            switch($x) {
-            case 0:
-                echo 'Saison 1 - Episode 01';
-                break;
-            case 1:
-                echo 'Saison 1 - Episode 02';
-                break;
-            case 2:
-                echo 'Saison 1 - Episode 03';
-                break;
-            case 3:
-                echo 'Saison 1 - Episode 04';
-                break;
-            case 4:
-                echo 'Saison 1 - Episode 05';
-                break;
-            default:
-                echo 'bug pas ça marche pas lol';
-            }
-        }
-</script>
-    
-
-<form>
-<input type="button" value="Démarrer la machine">
-</form>
-
-
-<p>test </p>
-
-
-
-    <script>
-        var btn = document.querySelector('input');
-        var txt = document.querySelector('p');
-
-
-        function updateBtn() {
-            switch() {
-            case 0:
-                echo 'Saison 1 - Episode 01';
-                break;
-            case 1:
-                echo 'Saison 1 - Episode 02';
-                break;
-            case 2:
-                echo 'Saison 1 - Episode 03';
-                break;
-            case 3:
-                echo 'Saison 1 - Episode 04';
-                break;
-            case 4:
-                echo 'Saison 1 - Episode 05';
-                break;
-            default:
-                echo 'bug pas ça marche pas lol';
-            }
-        }
-    </script>
-
-  
-
